e5241720610484717f8b9e76628f512476ccfa17
jeonyunhoo <jeonyunhu0130@gmail.com>
2026-08-19 21:17:33 +0900

    Spring boot 실습

---
+++ Springboot-practice2/src/main/java/com/example/springbootpractice2/SpringbootPractice2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPractice2Application.class, args);
+    }
+
+}
--- Springboot-practice2/src/main/java/com/example/springbootpractice2/domain/Todo.java
+++ Springboot-practice2/src/main/java/com/example/springbootpractice2/domain/Todo.java
@@ -1,4 +1,53 @@
 package com.example.springbootpractice2.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue
+    private long privateCode; // 함부로 접근하지 못하도록 private로 설정
+
+    @Column(length=255)
+    private String todoDetail;
+
+    private boolean check;
+
+    public Todo() {
+
+    }
+
+    public long getPrivateCode() {
+
+        return this.privateCode;
+    }
+
+    public void setPrivateCode(long privateCode) {
+
+        this.privateCode = privateCode;
+    }
+
+    public String getTodoDetail() {
+
+        return this.todoDetail;
+    }
+
+    public void setTodoDetail(String todoDetail) {
+
+        this.todoDetail = todoDetail;
+    }
+
+    public boolean isCheck() {
+
+        return this.check;
+    }
+
+    public void setCheck(boolean check) {
+
+        this.check = check;
+    }
 }
