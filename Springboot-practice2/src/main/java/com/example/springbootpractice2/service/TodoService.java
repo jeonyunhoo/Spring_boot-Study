a27cb3246be34fcbb2940b39d7a231bb54caae5f
jeonyunhoo <jeonyunhu0130@gmail.com>
2026-08-22 21:44:26 +0900

    Spring boot 실습3

--- Springboot-practice2/src/main/java/com/example/springbootpractice2/service/TodoService.java
+++ Springboot-practice2/src/main/java/com/example/springbootpractice2/service/TodoService.java
@@ -1,8 +1,11 @@
 package com.example.springbootpractice2.service;
 
+import com.example.springbootpractice2.domain.Todo;
 import com.example.springbootpractice2.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
@@ -13,5 +16,15 @@ public class TodoService {
     public TodoService(TodoRepository todoRepository) {
 
         this.todoRepository = todoRepository;
+    }
+
+    public void saveTodo(Todo todo) {
+
+        todoRepository.save(todo);
+    }
+
+    public List<Todo> getAllTodos() {
+
+        return todoRepository.findAll();
     }
 }
